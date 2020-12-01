5b65152145f7fe20b9bc26656e56716a71f3e279
Alex Ribeiro Alves <alexlekito075@gmail.com>
2020-12-01 11:56:23 -0300

    Update index.js

--- Exercio_01/meu-app-react/src/componentes/Card/index.js
+++ Exercio_01/meu-app-react/src/componentes/Card/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './style.css'
 
 export default function Card() {
     return (
-        <div>
+        <div className="card-container">
             <p>Componentes:</p>
             <p>Facilita entender as partes menores funcionando corretamente</p>
             <p>Você pode reutilizá-los, ou seja, menos código para escrever</p>
