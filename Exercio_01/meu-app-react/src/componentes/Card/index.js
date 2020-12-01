92af1c82c482b9dead40fe31c98ccfbdba561974
Alex Ribeiro Alves <alexlekito075@gmail.com>
2020-12-01 10:00:21 -0300

    Create index.js

---
+++ Exercio_01/meu-app-react/src/componentes/Card/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Card() {
+    return (
+        <div>
+            <p>Componentes:</p>
+            <p>Facilita entender as partes menores funcionando corretamente</p>
+            <p>Você pode reutilizá-los, ou seja, menos código para escrever</p>
+        </div>
+    );
+}
