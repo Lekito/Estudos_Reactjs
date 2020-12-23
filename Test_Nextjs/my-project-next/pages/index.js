2c42976fc8b6273c3d162af63d1e4e819ddc20d5
Alex Ribeiro Alves <alexlekito075@gmail.com>
2020-12-23 20:48:58 -0300

    Create index.js

---
+++ Test_Nextjs/my-project-next/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return(
+    <body className="p-4 bg-white-300 text-center">
+      <header className="p-4 bg-purple-300 text-center text-lg">
+        <h1 >Hello Tailwindcss from next.js</h1>
+      </header>
+    </body>
+  ) 
+}
