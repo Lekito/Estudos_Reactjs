da67de832ca43e96e5dc562e7ff309bb8f366814
Alex Ribeiro Alves <alexlekito075@gmail.com>
2020-12-23 22:00:42 -0300

    Update index.js

--- Test_Nextjs/my-project-next/pages/index.js
+++ Test_Nextjs/my-project-next/pages/index.js
@@ -3,10 +3,24 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return(
-    <body className="p-4 bg-white-300 text-center">
-      <header className="p-4 bg-purple-300 text-center text-lg">
-        <h1 >Hello Tailwindcss from next.js</h1>
-      </header>
+    <body className="bg-indigo-100">
+      <div className="grid grid-cols-1 lg:grid-cols-2">
+        <div className="bg-blue-600 lg:min-h-screen lg:flex lg:items-center p-8 sm:p-12">
+          <div className="flex-grow"> 
+            <h1 className="text-white text-center text-2xl sm:text-5xl mb-2">
+              Seja bem-vindo(a)
+            </h1>
+            <p className="text-center text-blue-200 sm:text-lg">
+              Faça seu login para começar 
+            </p>
+          </div>
+        </div>
+        <div className="lg:min-h-screen lg:flex lg:items-center p-12 lg:p-24 xl:p-48">
+          <div className="flex-grow bg-white shadow-xl rounded-md border border-grey-300 p-8">
+              
+          </div>
+        </div>
+      </div>
     </body>
   ) 
 }
