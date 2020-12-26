262e70180ac711e72e75460ff7dda386a92e1dac
Alex Ribeiro Alves <alexlekito075@gmail.com>
2020-12-26 17:51:08 -0300

    Update index.js

--- Test_Nextjs/my-project-next/pages/index.js
+++ Test_Nextjs/my-project-next/pages/index.js
@@ -20,13 +20,7 @@ export default function Home() {
         <div className="lg:min-h-screen lg:flex lg:items-center p-12 lg:p-24 xl:p-48">
           <div className="flex-grow bg-white shadow-xl rounded-md border border-grey-300 p-8">
             <div className="sm:flex sm:items-center">
-              <Image 
-                src="/alex_pequena.jpg"
-                alt="Foto do autor Alex"
-                className="sm:flex-shrink-0 mx-auto sm:mx-0 h-24 rounded-full"
-                width={110}
-                height={110}
-              />
+              <img src="/alex_pequena.jpg" className="sm:flex-shrink-0 mx-auto sm:mx-0 h-20 rounded-full"/>
               <div className="sm:ml-4 sm:text-left text-center">
                 <p className="text-xl">Alex Ribeiro</p>
                 <p className="text-sm text-gray-600">Desenvolvedor Full Stack</p>
