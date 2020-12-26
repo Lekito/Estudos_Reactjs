e72cd22affffbad52403039c14f88db47bbcf04b
Alex Ribeiro Alves <alexlekito075@gmail.com>
2020-12-26 16:24:20 -0300

    Update index.js

--- Test_Nextjs/my-project-next/pages/index.js
+++ Test_Nextjs/my-project-next/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Image from 'next/image';
 
 
 export default function Home() {
@@ -19,8 +20,29 @@ export default function Home() {
         <div className="lg:min-h-screen lg:flex lg:items-center p-12 lg:p-24 xl:p-48">
           <div className="flex-grow bg-white shadow-xl rounded-md border border-grey-300 p-8">
             <div className="sm:flex sm:items-center">
-              
+              <Image 
+                src="/alex_pequena.jpg"
+                alt="Foto do autor Alex"
+                className="sm:flex-shrink-0 mx-auto sm:mx-0 h-24 rounded-full"
+                width={110}
+                height={110}
+              />
+              <div className="sm:ml-4 sm:text-left text-center">
+                <p className="text-xl">Alex Ribeiro</p>
+                <p className="text-sm text-gray-600">Desenvolvedor Full Stack</p>
+                <div className="mt-4">
+                  <button type="button" className="text-red-500 hover:text-white hover:bg-red-500 border border-red-500 font-semibold rounded-md text-xs px-4 py-1 focus:outline-none">
+                   Não é Alex
+                  </button>
+                </div>
+              </div>
             </div>
+            <form className="flex w-full mt-8">
+              <input type="password" placeholder="Sua senha" className="flex-1 w-full text-gray-700 bg-gray-200 rounded-md hover:bg-white border border-gray-200 outline-none focus:bg-white py-2 px-4"></input>
+              <botton type="button" className="flex-shrink-0 bg-green-500 hover:bg-green-700 outline-none py-2 px-4 ml-4 text-white font-semibold rounded-md ">
+                Entrar
+              </botton>
+            </form>
           </div>
         </div>
       </div>
