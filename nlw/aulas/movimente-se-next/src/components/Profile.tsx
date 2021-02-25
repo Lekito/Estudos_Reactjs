5ec9c859c8db8a5f035ccecac33b1c56bce4325d
Alex Ribeiro Alves <alexlekito075@gmail.com>
2021-02-25 18:14:41 -0300

    Update Profile.tsx

--- nlw/aulas/movimente-se-next/src/components/Profile.tsx
+++ nlw/aulas/movimente-se-next/src/components/Profile.tsx
@@ -6,7 +6,10 @@ export function Profile() {
             <img src="https://github.com/Lekito.png" alt="Alex Ribeiro" />
             <div>
                 <strong>Alex Ribeiro</strong>
-                <p>Level 1</p>
+                <p>
+                    <img src="icons/level.svg" alt="Level" />
+                    Level 1
+                </p>
             </div>
         </div>
     );
