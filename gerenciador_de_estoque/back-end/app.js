926245ee64d319c77e3d241cda74acf6f5c3a308
Lekito <alexlekito075@gmail.com>
2024-02-07 10:13:25 -0300

    Create middleware and conect to back-end

--- gerenciador_de_estoque/back-end/app.js
+++ gerenciador_de_estoque/back-end/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const Produto = require('./models/Produtos');
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", ['GET', 'PUT', 'POST', 'DELETE']);
+    res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type, Authorization");
+    app.use(cors());
+    next();
+});
 
 app.get("/view-produto/:id", async (req, res) => {
     const { id } = req.params;
