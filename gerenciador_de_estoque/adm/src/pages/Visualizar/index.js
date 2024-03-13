c048dc5722920a14643606339b8d740cad361e68
Lekito <alexlekito075@gmail.com>
2024-03-13 19:04:10 -0300

    connects Visualizar page with the back-end

--- gerenciador_de_estoque/adm/src/pages/Visualizar/index.js
+++ gerenciador_de_estoque/adm/src/pages/Visualizar/index.js
@@ -3,21 +3,38 @@ import { Menu } from '../../Components/Menu';
 import { Link, useParams } from 'react-router-dom';
 import { Container, ConteudoTitulo, Titulo, BotaoAcao, ButtonWarning, ButtonInfo, ConttentView, Hr } from "../../styles/custom_adm";
 
+import api from '../../config/configApi';
+
 export const Visualizar = () => {
 
     const { id } = useParams();
     console.log(id);
 
     const [data, setData] = useState("");
+    const [status, setStatus] = useState({
+        type: "",
+        mensagem: ""
+    });
 
     useEffect(() => {
         const getProduto = async () => {
-            setData({
-                id: 1,
-                nome: "Mouse",
-                valor: 52.47,
-                quantidade: 43
-            })
+
+            await api.get("/view-produto/" + id)
+                .then((response) => {
+                    setData(response.data.produto);
+                }).catch((err) => {
+                    if (err.response) {
+                        setStatus({
+                            type: "error",
+                            mensagem: err.response.data.message
+                        });
+                    } else {
+                        setStatus({
+                            type: "error",
+                            mensagem: "Erro: Tente mais tarde!"
+                        });
+                    }
+                });
         }
         getProduto();
     }, [id]);
@@ -35,10 +52,10 @@ export const Visualizar = () => {
 
             <Hr />
 
-            <ConttentView>Selecionado: {id}</ConttentView>
             <ConttentView>ID:{data.id}</ConttentView>
             <ConttentView>Nome: {data.nome}</ConttentView>
-            <ConttentView>Valor: {data.valor}</ConttentView>
+            <ConttentView>Preço de Compra: R$ {data.preco_compra}</ConttentView>
+            <ConttentView>Preço de Venda: R$ {data.preco_venda}</ConttentView>
             <ConttentView>Quantidade: {data.quantidade}</ConttentView>
         </Container>
     );
